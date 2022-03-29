e8ecbf40f77a36b0af2d6d74da5ab3dc0dbc767e
iamnotfelix <andrei.iliescu03@gmail.com>
2022-03-29 13:21:05 +0300

    added specifications for the user services class

--- linkMaster/userServices.h
+++ linkMaster/userServices.h
@@ -11,11 +11,38 @@ private:
 
 public:
 	UserServices(TutorialRepo& repo, TutorialRepo& watchList);
+	/*
+	* Returns a dynamic vector containing all the tutorials from the repo that match the given presenter.
+	*	presenter - the presenter to be matched
+	*/
 	DynamicVector<Tutorial> getTutorialsByPresenter(const std::string presenter="");
+
+	/*
+	* Adds a tutorial to the watchlist.
+	*	tutorial - the tutorial to be added
+	*/
 	void addToWatchList(const Tutorial& tutorial);
+	
+	/*
+	* Deletes a tutorial from the watch list.
+	*	tutorial - the tutorial to be deleted
+	*/
 	void deleteFromWatchList(const Tutorial& tutorial);
+	
+	/*
+	* Likes a given tutorial from the repository.
+	*	tutorial - the tutorial to be liked
+	*/
 	void likeTutorial(const Tutorial& tutorial);
+
+	/*
+	* Returns the size of the watch list.
+	*/
 	int getWatchListSize();
+
+	/*
+	* Returns a dynamic vector containing the watch list.
+	*/
 	DynamicVector<Tutorial> getWatchList();
 };
 
